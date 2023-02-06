e547d0f5ff007a40d702934e0c9dd4a054e0abe8
Mohammed Yahya S <yahyamdmail@gmail.com>
2023-02-06 17:28:02 +0530

    side bar

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,17 @@
+import axois from "axios";
+
+const Base_Url = "https://youtube-v31.p.rapidapi.com/";
+
+const options = {
+  params: { part: "snippet", videoId: "M7FIvfx5J10" },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axois.get(`${Base_Url}/${url}`, options);
+
+  return data;
+};
